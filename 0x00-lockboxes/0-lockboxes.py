d2fa82c90852c246e78ff8b1966ab0705bf5cd23
bullbearan <4403@holbertonstudents.com>
2022-12-08 22:36:57 +0100

    first commit

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -6,6 +6,6 @@ def canUnlockAll(boxes):
     keys = [0]
     for i in keys:
         for j in boxes[i]:
-            if j not in keys:
+            if j not in keys and j < len(boxes):
                 keys.append(j)
     return len(keys) == len(boxes)
