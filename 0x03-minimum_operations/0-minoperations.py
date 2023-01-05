cad51ed7977f9ca30325864963918d80dc14c1da
bullbearan <4403@holbertonstudents.com>
2023-01-05 08:29:29 +0100

    add

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3                                                                                                                              
+"""
+Main file for testing
+"""
+def minOperations(n):                                                                                                                           
+    "This is a line of text"
+    if n < 2:                                                                                                                               
+        return 0                                                                                                                        
+    res, fac = 0, 2                                                                                                                         
+    while n > 1:                                                                                                                            
+        while n % fac == 0:                                                                                                             
+            res += fac                                                                                                              
+            n /= fac                                                                                                                
+        fac += 1                                                                                                                        
+    return res
