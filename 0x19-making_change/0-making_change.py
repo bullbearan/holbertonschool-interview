54530276d528a8dea197368c22205fbfdf97ff7c
bullbearan <4403@holbertonstudents.com>
2023-07-06 09:20:27 +0100

    add

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"This is a line of text"
+
+
+def makeChange(coins, total):
+    "This is a line of text"
+    if total <= 0:
+        return 0
+    if total in coins:
+        return 1
+    current_sum = 0
+    res = 0
+    coins.sort(reverse=True)
+    for coin in coins:
+        while current_sum < total:
+            current_sum += coin
+            res += 1
+        if current_sum == total:
+            return res
+        current_sum -= coin
+        res -= 1
+    return -1
