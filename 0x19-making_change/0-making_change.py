4214ac36d38158d8ffe63dbc13dc2965a4bef02c
bullbearan <4403@holbertonstudents.com>
2023-07-06 11:13:21 +0100

    add

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -8,12 +8,12 @@ def makeChange(coins, total):
         return 0
     if len(coins) <= 0:
         return -1
-    res = [total + 1] * (total + 1)
-    res[0] = 0
+    r = [total + 1] * (total + 1)
+    r[0] = 0
     for i in range(1, total + 1):
-        for coin in coins:
-            if i >= coin:
-                res[i] = min(res[i], res[i - coin] + 1)
+        for c in coins:
+            if i >= c:
+                r[i] = min(r[i], r[i - c] + 1)
             else:
                 break
-    return -1 if res[total] == total + 1 else res[total]
+    return r[total] if r[total] != total + 1 else -1
