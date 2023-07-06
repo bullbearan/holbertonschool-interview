a9200b82451e5a81cef379c003d47464a59e15a6
bullbearan <4403@holbertonstudents.com>
2023-07-06 10:47:34 +0100

    add

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -6,6 +6,8 @@ def makeChange(coins, total):
     "This is a line of text"
     if total <= 0:
         return 0
+    if len(coins) <= 0:
+        return -1
     res = [total + 1] * (total + 1)
     res[0] = 0
     for i in range(1, total + 1):
