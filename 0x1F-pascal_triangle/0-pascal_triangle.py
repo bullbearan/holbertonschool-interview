cd03879872ccfe9af11a6607f893ecd0c13089d7
bullbearan <107461834+bullbearan@users.noreply.github.com>
2023-08-16 21:50:04 +0100

    add

---
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"Titans aeotnas etoa"
+
+def pascal_triangle(n):
+    "Titans aeotnas etoa"
+    if n <= 0:
+        return []
+
+    pascal = [[1]]
+    while len(pascal) != n:
+        prev = pascal[-1]
+        current = [1]
+        for i in range(len(prev) - 1):
+            current.append(prev[i] + prev[i + 1])
+        current.append(1)
+        pascal.append(current)
+    return pascal
