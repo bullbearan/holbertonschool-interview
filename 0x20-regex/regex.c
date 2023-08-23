45153aee7d3f096751fbfb5152f6dce4b315e8d6
bullbearan <107461834+bullbearan@users.noreply.github.com>
2023-08-23 11:50:56 +0100

    add

---
+++ 0x20-regex/regex.c
@@ -0,0 +1,23 @@
+#include "regex.h"
+
+/**
+ * regex_match - function
+ * @str: para
+ * @pattern: para
+ * Return: return
+ */
+int regex_match(char const *str, char const *pattern)
+{
+	if (*str == '\0' && *pattern == '\0')
+		return (1);
+	if (*(pattern + 1) == '*')
+	{
+		if (*str != '\0' && (*str == *pattern || *pattern == '.'))
+			return (regex_match(str, pattern + 2) ||
+					regex_match(str + 1, pattern));
+		return (regex_match(str, pattern + 2));
+	}
+	if ((*str == *pattern || *pattern == '.'))
+		return (regex_match(str + 1, pattern + 1));
+	return (0);
+}
